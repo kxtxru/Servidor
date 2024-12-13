230b59070e2424811c4def48f7d179c4b3d5fe25
alvagumar <alvagumar@gva.alu.edu.es>
2024-12-13 13:19:16 +0100

    1.2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 
 public class Main {
 
-    private static final String host = "192.168.67.173";
+    private static final String host = "localhost";
     private static final int port = 1337;
     public static void main(String[] args) {
     
-        // //Parte SERVIDOR
-        // Server srv = new Server(port);
-        // Thread tServer = new Thread(srv);
-        // tServer.start();
-        // try {
-        //     Thread.sleep(1000);
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
+        //Parte SERVIDOR
+        Server srv = new Server(port);
+        Thread tServer = new Thread(srv);
+        tServer.start();
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         //Parte CLIENTE
         Cliente c = new Cliente(host, port);
